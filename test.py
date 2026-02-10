f0987e74871f714877e7c269760aa543d1a7050f
127001 <ybkyal@qq.com>
2026-02-10 15:56:24 +0800

    修改

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ pygame.init()
 pygame.display.set_caption("Title")
 screen = pygame.display.set_mode((500, 300))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    
-    screen.fill("white")
-    pygame.display.flip()
+if __name__ == '__main__':
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        screen.fill("white")
+        pygame.display.flip()
