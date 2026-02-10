b81ecc151cd50895d49b5a7184a447d7bf8bd676
127001 <ybkyal@qq.com>
2026-02-10 15:32:18 +0800

    test pygame

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-print('hello world')
-print('你好，世界')
-print('Hello, World!')
+import pygame
+import sys
+
+pygame.init()
+
+pygame.display.set_caption("Title")
+screen = pygame.display.set_mode((500, 300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+    screen.fill("white")
+    pygame.display.flip()
